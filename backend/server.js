6e2066cf74fdd885296edfb177b7fafbc3036587
Glauco Bernardes - CGR <g.bernardes@plaenge.com.br>
2026-04-11 13:35:16 -0400

    teste servidor simples

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,12 @@
 const express = require('express');
 const app = express();
-//const db = require('./config/db');
-
-app.use(express.json());
-
-app.get('/test-db', (req, res) => {
-  db.query('SELECT 1', (err, result) => {
-    if (err) return res.send(err);
-    res.send('Banco conectado 🚀');
-  });
-});
 
 app.get('/', (req, res) => {
-  res.send('API Jovens Unidos rodando 🚀');
-});
-
-app.get('/jogadores', (req, res) => {
-  db.query('SELECT * FROM jogadores', (err, result) => {
-    if (err) return res.status(500).json(err);
-    res.json(result);
-  });
+  res.send('API rodando 🚀');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Servidor rodando na porta ' * PORT);
+  console.log('Servidor rodando na porta ' + PORT);
 });
