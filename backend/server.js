6a25f6aaf0c96410eb8ab55334a3b3c316197b09
Glauco Bernardes - CGR <g.bernardes@plaenge.com.br>
2026-04-11 13:05:50 -0400

    config porta correta

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ app.get('/jogadores', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta ' * PORT);
 });
