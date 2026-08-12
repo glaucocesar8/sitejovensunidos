94172b2d570639d78dae3a00e654fbdf4f926188
Glauco Bernardes - CGR <g.bernardes@plaenge.com.br>
2026-08-11 21:16:11 -0400

    Configura Express para Vercel

--- api/index.js
+++ api/index.js
@@ -1,15 +1,3 @@
 const app = require('../server');
 
-module.exports = (req, res) => {
-
-  // Remove /api da URL antes de entregar
-  // a requisição ao Express.
-
-  if (req.url.startsWith('/api')) {
-    req.url =
-      req.url.substring(4) || '/';
-  }
-
-  return app(req, res);
-
-};
+module.exports = app;
